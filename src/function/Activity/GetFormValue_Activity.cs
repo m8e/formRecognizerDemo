adf0028b13d7cb003179c2be57051a3fc3e3d5d2
Hugo Girard <hugogirard28@hotmail.com>
2021-06-07 10:57:16 -0400

    remove orchestrator function

--- src/function/Activity/GetFormValue_Activity.cs
+++ src/function/Activity/GetFormValue_Activity.cs
@@ -1,45 +1,45 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using Azure.AI.FormRecognizer;
-using Azure.AI.FormRecognizer.Models;
-using System.Net.Http;
-using System.Linq;
-using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+//using System;
+//using System.IO;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Azure.WebJobs;
+//using Microsoft.Azure.WebJobs.Extensions.Http;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.Extensions.Logging;
+//using Newtonsoft.Json;
+//using Azure.AI.FormRecognizer;
+//using Azure.AI.FormRecognizer.Models;
+//using System.Net.Http;
+//using System.Linq;
+//using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 
-namespace Contoso
-{
-    public class GetFormValue
-    {
-        private readonly FormRecognizerClient _formRecognizer;
+//namespace Contoso
+//{
+//    public class GetFormValue
+//    {
+//        private readonly FormRecognizerClient _formRecognizer;
         
-        public GetFormValue(FormRecognizerClient client)
-        {
-            _formRecognizer = client;            
-        }
+//        public GetFormValue(FormRecognizerClient client)
+//        {
+//            _formRecognizer = client;            
+//        }
 
-        [FunctionName("GetFormValue")]
-        public async Task<string> SayHello([ActivityTrigger] Payload payload, ILogger log)
-        {
-            try
-            {
-                RecognizedFormCollection forms = await _formRecognizer.StartRecognizeCustomFormsFromUri(payload.ModelId,
-                                                                                                        new Uri(payload.FileUri))
-                                                                      .WaitForCompletionAsync();
+//        [FunctionName("GetFormValue")]
+//        public async Task<string> SayHello([ActivityTrigger] Payload payload, ILogger log)
+//        {
+//            try
+//            {
+//                RecognizedFormCollection forms = await _formRecognizer.StartRecognizeCustomFormsFromUri(payload.ModelId,
+//                                                                                                        new Uri(payload.FileUri))
+//                                                                      .WaitForCompletionAsync();
 
-                return JsonConvert.SerializeObject(forms);
-            }
-            catch (System.Exception ex)
-            {
-                log.LogError(ex.Message,ex);
-                throw;
-            }
-        }
-    }
-}
+//                return JsonConvert.SerializeObject(forms);
+//            }
+//            catch (System.Exception ex)
+//            {
+//                log.LogError(ex.Message,ex);
+//                throw;
+//            }
+//        }
+//    }
+//}
--- src/function/Activity/SendAnswer_Activity.cs
+++ src/function/Activity/SendAnswer_Activity.cs
@@ -1,38 +1,38 @@
-﻿using Contoso;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.DurableTask;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using Contoso;
+//using Microsoft.Azure.WebJobs;
+//using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+//using Microsoft.Extensions.Logging;
+//using System;
+//using System.Collections.Generic;
+//using System.Net.Http;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace function
-{
-    public class SendAnswer_Activity
-    {
-        private readonly IHttpClientFactory _httClientFactory;
+//namespace function
+//{
+//    public class SendAnswer_Activity
+//    {
+//        private readonly IHttpClientFactory _httClientFactory;
 
-        public SendAnswer_Activity(IHttpClientFactory clientFactory)
-        {
-            _httClientFactory = clientFactory;
-        }
+//        public SendAnswer_Activity(IHttpClientFactory clientFactory)
+//        {
+//            _httClientFactory = clientFactory;
+//        }
 
 
-        [FunctionName("SendAnswer")]
-        public async Task SendAnswerToWebhook([ActivityTrigger] Response answer, ILogger log)
-        {
-            try
-            {
-                var httpClient = _httClientFactory.CreateClient();
-                await httpClient.PostAsJsonAsync(answer.WebHookUri,answer.Forms);
-            }
-            catch (System.Exception ex)
-            {
-                log.LogError(ex.Message, ex);
-                throw;
-            }
-        }
-    }
-}
+//        [FunctionName("SendAnswer")]
+//        public async Task SendAnswerToWebhook([ActivityTrigger] Response answer, ILogger log)
+//        {
+//            try
+//            {
+//                var httpClient = _httClientFactory.CreateClient();
+//                await httpClient.PostAsJsonAsync(answer.WebHookUri,answer.Forms);
+//            }
+//            catch (System.Exception ex)
+//            {
+//                log.LogError(ex.Message, ex);
+//                throw;
+//            }
+//        }
+//    }
+//}
